7dc686b5902abf47adf934141bee1a4166950f9f
davidperlaza14 <davidperlaza14@gmail.com>
2023-02-20 10:29:12 -0500

    introduccion

---
+++ Introducción-a-la-programacion/Funciones3/Usos.java
@@ -0,0 +1,6 @@
+// package Funciones3;
+
+// Las funciones 'empaquetan' y aislan del resto del programa una serie de variables e instrucciones de código que realizan alguna tarea específica. Solo se ejecutan si son llamadas desde el código principal y tras procesar sus instrucciones, devuelven un resultado a esa parte del código principal que la invocó.
+// public class Usos {
+  
+// }
---
+++ Introducción-a-la-programacion/Funciones3/main.java
@@ -0,0 +1,18 @@
+// Usos
+
+
+
+// package Funciones3;
+
+// public class main {
+//   public static void main(String[] args) {
+//     suma(10, 70);  
+//   }
+
+//   public static void suma(int a, int b) {
+//     int resultado;
+//     resultado = a + b;
+
+//     System.out.println(resultado);
+//   }
+// }
