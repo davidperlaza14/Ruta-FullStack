34991419ff70725e435de3a72722d9afd3d91b30
davidperlaza14 <davidperlaza14@gmail.com>
2023-02-21 16:13:28 -0500

    Control de flujo

---
+++ Python/4_Control_de_flujo/ejercicio/ejercicio.py
@@ -0,0 +1,9 @@
+# Enunciado del ejercicio:
+
+# Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso
+
+contador = 100
+
+while contador >= 1:
+  print(contador, end=",")
+  contador -= 1
---
+++ Python/4_Control_de_flujo/main.py
@@ -0,0 +1,138 @@
+# Condicional
+
+# a = 5
+# b = 6
+# c = 7
+
+# > Mayor
+# < Menor
+# >= Mayor o igual
+# <= Mneor o igual
+
+# resultado = a < b
+# print(resultado)
+
+# resultado = a > b
+# print(resultado)
+
+# resultado = a >= b
+# print(resultado)
+
+# resultado = (a == b)
+# print(resultado)
+
+# resultado = (a >= 5 and c >= b)
+# print(resultado)
+
+
+# resultado = (a >= 5 or  c < b)
+# print(resultado)
+
+
+# resultado = ((a >= 5 or  c > 7) and (b == 5))
+# print(resultado)
+
+
+# if a > 5 and b <= 6:
+#   print("A es mayor o igual que 5 y B es menor o igual que 6")
+# elif a >= 6:
+#   print("A es mayor o igual que 6")
+
+# else:
+#   print("No se cumple NADA de lo anterior")
+  
+# ================================================
+
+# BUCLE CONDICIONAL
+
+# contador = 1
+# while contador <= 10:
+#   if contador % 2 == 0:
+#     print(contador, " es par")
+#   print("contador vale: ", contador)
+#   contador += 1
+# print("Fin del bucle.")
+
+
+# Palabra reservadas Break y continue
+
+
+# contador = 1
+
+# while contador <= 10:
+#     print("contador vale: ", contador)
+    
+#     if contador == 5:
+#       print("Rompo el buble")
+#       break
+    
+#     contador += 1
+  
+# print("Fin del bucle.")
+
+
+
+# contador = 0
+
+# while contador <= 10:
+#   if contador % 2 == 0:
+#     print(contador, " es par")
+#     # continue
+  
+#   print("Y ahora incrementa el contador")
+#   contador += 1
+  
+# print("Fin del bucle.")
+
+# ===================================
+
+# Bucle For
+
+# lista = [1, 2, 3, 4, 5]
+# tupla = (1, 2, 3, 'c', 'd')
+# for valorActual in lista:
+#   print(valorActual)
+  
+  
+
+# tupla = (1, 2, 3, 'c', 'd')
+
+# for numero in range(5, 10):
+#   print(numero)
+
+
+# lista = ["Hola", "mensaje", "adios"]
+
+# for palabra in lista:
+#   print("Palabra actual: ", palabra)
+  
+#   if palabra == "mensaje":
+#     print("He encontrado la palabra mensaje")
+#     break
+  
+
+# if "mensaje" in lista:
+#   print("He encontrado la palabra mensaje")
+  
+  
+# lista = [3, 4, 1, 2, 5]
+# print(lista)
+
+# listaOrdenada = sorted(lista)
+# print(listaOrdenada)
+
+# lista = ['A','a','b','B','C', 'c']
+# print(lista)
+
+# listaOrdenada = sorted(lista)
+# print(listaOrdenada)
+
+
+# =====================
+
+# Match
+
+# ===========
+  
+  
+
