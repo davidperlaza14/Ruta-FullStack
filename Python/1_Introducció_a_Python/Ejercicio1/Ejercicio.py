116adfc0f7a7a8de2421f5f346a3cd620f72089e
davidperlaza14 <davidperlaza14@gmail.com>
2023-02-21 10:17:32 -0500

    Update

--- Python/1_Introducció_a_Python/Ejercicio1/Ejercicio.py
+++
@@ -1,5 +0,0 @@
-# Enunciado del ejercicio:
-
-# Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
-
-# Tienes que subir capturas de pantalla en una carpeta comprimida zip.
